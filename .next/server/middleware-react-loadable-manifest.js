3ff86d7ca365d0607a60bed60e4f4b6e68a9c4fe
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-09 13:24:52 +0100

    core: create project

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> ./App\":{\"id\":\"app/page.tsx -> ./App\",\"files\":[\"static/chunks/_app-pages-browser_src_app_App_tsx.js\"]}}"
---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+import './index.css';
+
+export const metadata: Metadata = {
+  title: 'Tennis matches'
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
