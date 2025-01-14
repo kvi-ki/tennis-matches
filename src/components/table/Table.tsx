9f38e4d4f69f84b68d96ab0f8e1eabb5735640ac
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-14 14:19:23 +0100

    feat: add table headers

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,3 +1,15 @@
 export default function Table() {
-  return <table></table>;
+  return (
+    <table>
+      <tr>
+        <th>Jugador</th>
+        <th>PJ</th>
+        <th>PG</th>
+        <th>PP</th>
+        <th>JF</th>
+        <th>JC</th>
+        <th>Dif</th>
+      </tr>
+    </table>
+  );
 }
