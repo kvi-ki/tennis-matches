1427099a993382083a099ea324955d47545d945c
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-14 11:26:45 +0100

    style: add font and color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-montserrat">
         <div id="root">{children}</div>
       </body>
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        "montserrat": ['Montserrat', 'serif']
+      }
+    },
+    colors: {
+      navy: '#0461a6'
+    },
+    fontSize: {
+      header: '2rem'
+    }
+  },
   plugins: []
 }
