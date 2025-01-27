5e213b1a20277e6c09817fa9e9f8109333e2b1cb
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-27 16:05:02 +0100

    feat: add the rest of player's data

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -12,6 +12,12 @@ export default function Player(player: PlayerProps) {
   return (
     <tr>
       <td>{player.player}</td>
+      <td>{player.pj}</td>
+      <td>{player.pg}</td>
+      <td>{player.pp}</td>
+      <td>{player.jf}</td>
+      <td>{player.jc}</td>
+      <td>{player.dif}</td>
     </tr>
   );
 }
--- tests/unit/player.test.tsx
+++ tests/unit/player.test.tsx
@@ -6,7 +6,11 @@ describe('Player', () => {
     render(<Table />);
 
     const playerName = screen.getByText('Marc');
+    const playerPJ = screen.getByText('4');
+    const playerJC = screen.getByText('20');
 
     expect(playerName).toBeVisible();
+    expect(playerPJ).toBeVisible();
+    expect(playerJC).toBeVisible();
   });
 });
