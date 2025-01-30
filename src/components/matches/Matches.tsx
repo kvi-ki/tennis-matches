9398a3fc8f119d66b61ebd0df71b886911f139ad
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-30 16:38:35 +0100

    feat: toggle the list

--- src/components/matches/Matches.tsx
+++ src/components/matches/Matches.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Match, { MatchProps } from '../match/Match';
 
 export default function Matches({ matches }: { matches: MatchProps[] }) {
+  const [matchesAreHidden, setMatchesAreHidden] = useState(true);
+
+  const toggleMatches = () => {
+    setMatchesAreHidden((prev) => !prev);
+  };
+
   return (
     <section className="border border-navy mt-4 w-full md:w-8/12 xl:w-5/12">
-      <h2 className="border-b text-navy text-header2 text-center text-semibold p-1 lg:text-header2Lg">
+      <h2
+        className="border-b text-navy text-header2 text-center text-semibold p-1 lg:text-header2Lg"
+        onClick={toggleMatches}
+      >
         Partidos
       </h2>
-      <ul>
+      <ul className={matchesAreHidden ? 'hidden' : 'visible'}>
         {matches.map((match: MatchProps, index: number) => {
           return <Match match={match} key={index} />;
         })}
