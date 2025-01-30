36de3fb8790685aa17b6ad7787e92bdf7b5cd50a
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-30 16:00:19 +0100

    style: add html entity to players matches

--- src/components/match/Match.tsx
+++ src/components/match/Match.tsx
@@ -6,8 +6,9 @@ export type MatchProps = {
 
 export default function Match({ match }: { match: MatchProps }) {
   return (
-    <li className="p-1 text-gray text-textMatches font-semibold md:text-textMd lg:text-textLg">
-      {match.player1} vs {match.player2} {match.result}
+    <li className="p-1.5 ml-6 text-gray text-textMatches font-semibold md:text-textMd lg:text-textLg">
+      <span className="m-2">&#9643;</span> {match.player1} vs {match.player2}{' '}
+      {match.result}
     </li>
   );
 }
