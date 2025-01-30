ddd1c138cf1d5b1ff32fe952ea49cd19a4c3de93
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-30 12:55:46 +0100

    feat: add Matches component

--- src/components/divisionCard/DivisionCard.tsx
+++ src/components/divisionCard/DivisionCard.tsx
@@ -2,6 +2,7 @@ import { LeagueDataProps } from '../leagueData/LeagueData';
 import Header from '../header/Header';
 import Table from '../table/Table';
 import { PlayerProps } from '../player/Player';
+import Matches from '../matches/Matches';
 
 export default function DivisionCard({ league }: { league: LeagueDataProps }) {
   const divisionNumber: string = league.division;
@@ -11,6 +12,7 @@ export default function DivisionCard({ league }: { league: LeagueDataProps }) {
     <div className="m-4 flex flex-col items-center">
       <Header divisionNumber={divisionNumber} />
       <Table players={players} />
+      <Matches />
     </div>
   );
 }
