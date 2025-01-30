d2d44183857747809f24865d04e88939440910b5
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-30 12:57:12 +0100

    test: should render matches header

---
+++ tests/unit/matches.test.tsx
@@ -0,0 +1,12 @@
+import Matches from '@/components/matches/Matches';
+import { render, screen } from '@testing-library/react';
+
+describe('Matches', () => {
+  it('should render matches header', () => {
+    render(<Matches />);
+
+    const matchesHeading = screen.getByRole('heading', { level: 2 });
+
+    expect(matchesHeading).toBe('Partidos');
+  });
+});
