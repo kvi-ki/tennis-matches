3936cae448ed8fdff3afecf8878b6233c5718c61
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-30 16:39:07 +0100

    style: add changes on hover

--- src/components/matches/Matches.tsx
+++ src/components/matches/Matches.tsx
@@ -11,7 +11,7 @@ export default function Matches({ matches }: { matches: MatchProps[] }) {
   return (
     <section className="border border-navy mt-4 w-full md:w-8/12 xl:w-5/12">
       <h2
-        className="border-b text-navy text-header2 text-center text-semibold p-1 lg:text-header2Lg"
+        className="border-b text-navy text-header2 text-center text-semibold p-1 hover:text-green cursor-pointer lg:text-header2Lg"
         onClick={toggleMatches}
       >
         Partidos
