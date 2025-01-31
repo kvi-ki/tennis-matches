744810e4924c0bb1ed5e43ba65f83edd9a00cc2e
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-01-31 17:04:58 +0100

    test: should calculate player statistic

--- tests/unit/table.test.tsx
+++ tests/unit/table.test.tsx
@@ -2,6 +2,7 @@ import data from '../../mock.json';
 import Table from '@/components/table/Table';
 import { PlayerProps } from '@/components/player/Player';
 import { render, screen } from '@testing-library/react';
+import { LeagueDataProps } from '@/components/leagueData/LeagueData';
 
 describe('Table', () => {
   it('should render column headers', () => {
@@ -40,5 +41,54 @@ describe('Table', () => {
     const rows = screen.queryAllByRole('row');
 
     expect(rows.length).toBeGreaterThan(1);
+  });
+
+  it('should calculate player statistic', () => {
+    const league: LeagueDataProps = {
+      division: '1',
+      data: [
+        {
+          player: 'Marc',
+          pj: '',
+          pg: '',
+          pp: '',
+          jf: '',
+          jc: '',
+          dif: ''
+        },
+        {
+          player: 'Henry',
+          pj: '',
+          pg: '',
+          pp: '',
+          jf: '',
+          jc: '',
+          dif: ''
+        }
+      ],
+      matches: [
+        {
+          player1: 'Marc',
+          player2: 'Henry',
+          result: '9 - 7'
+        }
+      ]
+    };
+
+    //player1
+    expect(league.data[0].pj).toBe(1);
+    expect(league.data[0].pg).toBe(1);
+    expect(league.data[0].pp).toBe(0);
+    expect(league.data[0].jf).toBe(9);
+    expect(league.data[0].jc).toBe(7);
+    expect(league.data[0].dif).toBe(2);
+
+    //player2
+    expect(league.data[0].pj).toBe(1);
+    expect(league.data[0].pg).toBe(0);
+    expect(league.data[0].pp).toBe(1);
+    expect(league.data[0].jf).toBe(7);
+    expect(league.data[0].jc).toBe(9);
+    expect(league.data[0].dif).toBe(-2);
   });
 });
