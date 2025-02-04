9cc7b03d5990dd6928aaf66f8379dcab0a172896
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-02-04 15:02:25 +0100

    feat: add player summary calculations

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,13 +1,14 @@
+import { useEffect, useState } from 'react';
 import { MatchScoreProps } from '../match/Match';
 
 export type PlayerProps = {
   name: string;
-  pj: string;
-  pg: string;
-  pp: string;
-  jf: string;
-  jc: string;
-  dif: string;
+  pj: number;
+  pg: number;
+  pp: number;
+  jf: number;
+  jc: number;
+  dif: number;
 };
 
 export default function Player({
@@ -17,15 +18,47 @@ export default function Player({
   playerData: PlayerProps;
   matchScore: MatchScoreProps;
 }) {
+  const [playerSummary, setPlayerSummery] = useState<PlayerProps>(playerData);
+
+  if (!matchScore) return;
+
+  useEffect(() => {
+    if (matchScore.theWinner === matchScore.player1Name) {
+      return setPlayerSummery({
+        name: playerData.name,
+        pj: playerData.pj + 1,
+        pg: playerData.pg + 1,
+        pp: playerData.pp,
+        jf: playerData.jf + matchScore.player1Score,
+        jc: playerData.jc + matchScore.player2Score,
+        dif:
+          playerData.dif + (matchScore.player1Score - matchScore.player2Score)
+      });
+    }
+
+    if (matchScore.theWinner === matchScore.player2Name) {
+      return setPlayerSummery({
+        name: playerData.name,
+        pj: playerData.pj + 1,
+        pg: playerData.pg,
+        pp: playerData.pp + 1,
+        jf: playerData.jf + matchScore.player2Score,
+        jc: playerData.jc + matchScore.player1Score,
+        dif:
+          playerData.dif + (matchScore.player1Score - matchScore.player2Score)
+      });
+    }
+  }, [playerData, matchScore]);
+
   return (
     <tr>
-      <td className="font-semibold text-left pl-2">{playerData.name}</td>
-      <td>{playerData.pj}</td>
-      <td>{playerData.pg}</td>
-      <td>{playerData.pp}</td>
-      <td>{playerData.jf}</td>
-      <td>{playerData.jc}</td>
-      <td>{playerData.dif}</td>
+      <td className="font-semibold text-left pl-2">{playerSummary.name}</td>
+      <td>{playerSummary.pj}</td>
+      <td>{playerSummary.pg}</td>
+      <td>{playerSummary.pp}</td>
+      <td>{playerSummary.jf}</td>
+      <td>{playerSummary.jc}</td>
+      <td>{playerSummary.dif}</td>
     </tr>
   );
 }
