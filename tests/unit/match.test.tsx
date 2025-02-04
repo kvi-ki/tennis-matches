7a9d12a93b1d41d2060c15ad7bba6aabc5510c57
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-02-04 14:25:42 +0100

    test: update match tests

--- tests/unit/match.test.tsx
+++ tests/unit/match.test.tsx
@@ -2,11 +2,11 @@ import Match, { MatchProps } from '@/components/match/Match';
 import data from '../../mock.json';
 import { render, screen } from '@testing-library/react';
 
-const allMatches: MatchProps[][] = data.map((league) => league.matches);
-const matches = allMatches[0];
-const match = matches[0];
-
 describe('Match', () => {
+  const allMatches: MatchProps[][] = data.map((league) => league.matches);
+  const matches = allMatches[0];
+  const match = matches[0];
+
   it('should render players names', () => {
     render(<Match match={match} />);
 
@@ -18,7 +18,7 @@ describe('Match', () => {
   it('should render match score', () => {
     render(<Match match={match} />);
 
-    const score = screen.getByText(/9 - 7/);
+    const score = screen.getByText(/9-7/);
 
     expect(score).toBeInTheDocument();
   });
