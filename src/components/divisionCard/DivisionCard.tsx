8cedfa83abd140b60bb2b02346128ce555ad7b1d
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-02-11 15:23:48 +0100

    feat: use context provider to pass props

--- src/components/divisionCard/DivisionCard.tsx
+++ src/components/divisionCard/DivisionCard.tsx
@@ -4,32 +4,34 @@ import Table from '../table/Table';
 import { PlayerProps } from '../player/Player';
 import Matches from '../matches/Matches';
 import { MatchProps, MatchScoreProps } from '../match/Match';
-import { useState } from 'react';
+import { useState, createContext } from 'react';
+
+type MatchesContextType = {
+  matchesScores: MatchScoreProps[];
+  setMatchesScores: React.Dispatch<React.SetStateAction<MatchScoreProps[]>>;
+};
+
+export const MatchesGlobalStateContext = createContext<MatchesContextType>({
+  matchesScores: [],
+  setMatchesScores: () => {}
+});
 
 export default function DivisionCard({ league }: { league: LeagueDataProps }) {
+  const [matchesScores, setMatchesScores] = useState<MatchScoreProps[]>([]);
+
   const divisionNumber: string = league.division;
   const playersData: PlayerProps[] = league.playersData;
   const matches: MatchProps[] = league.matches;
-  const initialMatchesScores: MatchScoreProps[] = league.matches.map(
-    (match) => {
-      return {
-        player1Name: match.player1,
-        player2Name: match.player2,
-        player1Score: Number(match.score[0]),
-        player2Score: Number(match.score[match.score.length - 1]),
-        theWinner: ''
-      };
-    }
-  );
-
-  const [matchesScores, setMatchesScores] =
-    useState<MatchScoreProps[]>(initialMatchesScores);
 
   return (
-    <>
-      <Header divisionNumber={divisionNumber} />
-      <Table playersData={playersData} matchesScores={matchesScores} />
-      <Matches matches={matches} setMatchesScores={setMatchesScores} />
-    </>
+    <MatchesGlobalStateContext.Provider
+      value={{ matchesScores, setMatchesScores }}
+    >
+      <>
+        <Header divisionNumber={divisionNumber} />
+        <Table playersData={playersData} />
+        <Matches matches={matches} />
+      </>
+    </MatchesGlobalStateContext.Provider>
   );
 }
