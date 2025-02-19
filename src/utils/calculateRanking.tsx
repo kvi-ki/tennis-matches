b70b9db4009ff4bd51fd8c4aec60ce762b8dd54c
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-02-19 17:30:46 +0100

    chore: remove the util folder

--- src/utils/calculateRanking.tsx
+++
@@ -1,9 +0,0 @@
-import { PlayerProps } from "@/components/player/Player";
-
-export const calculateRanking = (playerTableData: PlayerProps) => {
-  const winningPoints: number = playerTableData.pg * 3;
-    const losingPoints: number = playerTableData.pp * 1;
-    const rankingPoints: number = winningPoints + losingPoints;
-
-    return rankingPoints;
-}
