ce145455efd7be07e1e36cb51a6658eb2efe8868
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-02-19 17:31:35 +0100

    feat: move players methods to table for simplier approach

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,8 +1,3 @@
-import { useEffect, useState } from 'react';
-import { MatchScoreProps } from '../match/Match';
-import { calculatePlayerPoints } from '@/utils/calculatePlayerPoints';
-import { calculateRanking } from '@/utils/calculateRanking';
-
 export type PlayerProps = {
   name: string;
   pj: number;
@@ -15,43 +10,22 @@ export type PlayerProps = {
 
 export default function Player({
   playerData,
-  matchScore
+  ranking
 }: {
   playerData: PlayerProps;
-  matchScore: MatchScoreProps[];
+  ranking: number
 }) {
-  const [playerTableData, setPlayerTableData] =
-    useState<PlayerProps>(playerData);
-
-  const [playerRanking, setPlayerRanking] = useState<number>(0);
-
-  if (!matchScore) {
-    return;
-  }
-
-  useEffect(() => {
-    setPlayerTableData((prev) => {
-      return calculatePlayerPoints(prev, matchScore);
-    });
-  }, [matchScore]);
-
-  useEffect(() => {
-    const rankingPoints: number = calculateRanking(playerTableData);
-    setPlayerRanking(rankingPoints);
-
-  }, [playerTableData]);
-
-  return (
+   return (
     <>
       <tr>
-        <td>{playerRanking}</td>
-        <td className="font-semibold text-left pl-2">{playerTableData.name}</td>
-        <td>{playerTableData.pj}</td>
-        <td>{playerTableData.pg}</td>
-        <td>{playerTableData.pp}</td>
-        <td>{playerTableData.jf}</td>
-        <td>{playerTableData.jc}</td>
-        <td>{playerTableData.dif}</td>
+        <td>{ranking}</td>
+        <td className="font-semibold text-left pl-2">{playerData.name}</td>
+        <td>{playerData.pj}</td>
+        <td>{playerData.pg}</td>
+        <td>{playerData.pp}</td>
+        <td>{playerData.jf}</td>
+        <td>{playerData.jc}</td>
+        <td>{playerData.dif}</td>
       </tr>
     </>
   );
--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,6 +1,7 @@
-import { JSX, useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import Player, { PlayerProps } from '../player/Player';
 import { MatchesGlobalStateContext } from '../divisionCard/DivisionCard';
+import { MatchScoreProps } from '../match/Match';
 
 export default function Table({ playersData }: { playersData: PlayerProps[] }) {
   const context = useContext(MatchesGlobalStateContext);
@@ -10,33 +11,98 @@ export default function Table({ playersData }: { playersData: PlayerProps[] }) {
   }
 
   const { matchesScores } = context;
-  const [playersDataToShow, setPlayersDataToShow] = useState<JSX.Element[]>([]);
-  
-  useEffect(() => {
-    const updatedPlayersData = playersData.map(
-      (playerData: PlayerProps, index: number) => {
-        const matchesPlayedByThisPlayer = matchesScores.filter(
-          (match) =>
-            match.player1Name === playerData.name ||
-            match.player2Name === playerData.name
-        );
 
-        const updatedMatches = matchesPlayedByThisPlayer.filter(
-          (match) => match.player1Score > 0 || match.player2Score > 0
-        );
-
-        return (
-          <Player
-            key={index}
-            playerData={playerData}
-            matchScore={updatedMatches}
-          />
-        );
-      }
+  const playersDataToShow = playersData.map((playerData) => {
+    const matchesPlayedByThisPlayer = matchesScores.filter(
+      (match) =>
+        match.player1Name === playerData.name ||
+        match.player2Name === playerData.name
     );
 
-    setPlayersDataToShow(updatedPlayersData);
-  }, [matchesScores]);
+    const updatedMatches = matchesPlayedByThisPlayer.filter(
+      (match) => match.player1Score > 0 || match.player2Score > 0
+    );
+
+    let newData = {
+      ...playerData,
+      pj: playerData.pj || 0,
+      pg: playerData.pg || 0,
+      pp: playerData.pp || 0,
+      jf: playerData.jf || 0,
+      jc: playerData.jc || 0,
+      dif: playerData.dif || 0
+    };
+
+    updatedMatches.forEach((match: MatchScoreProps) => {
+      const winnerPlayer1 =
+        match.theWinner === playerData.name &&
+        match.player1Name === playerData.name;
+      const winnerPlayer2 =
+        match.theWinner === playerData.name &&
+        match.player2Name === playerData.name;
+      const notWinnerPlayer1 =
+        match.theWinner !== playerData.name &&
+        match.player1Name === playerData.name;
+      const notWinnerPlayer2 =
+        match.theWinner !== playerData.name &&
+        match.player2Name === playerData.name;
+
+      if (winnerPlayer1) {
+        newData.pj += 1;
+        newData.pg += 1;
+        newData.pp;
+        newData.jf += match.player1Score;
+        newData.jc += match.player2Score;
+        newData.dif += match.player1Score - match.player2Score;
+      }
+
+      if (winnerPlayer2) {
+        newData.pj += 1;
+        newData.pg += 1;
+        newData.pp;
+        newData.jf += match.player2Score;
+        newData.jc += match.player1Score;
+        newData.dif += match.player2Score - match.player1Score;
+      }
+
+      if (notWinnerPlayer1) {
+        newData.pj += 1;
+        newData.pg;
+        newData.pp += 1;
+        newData.jf += match.player1Score;
+        newData.jc += match.player2Score;
+        newData.dif += match.player1Score - match.player2Score;
+      }
+
+      if (notWinnerPlayer2) {
+        newData.pj += 1;
+        newData.pg;
+        newData.pp += 1;
+        newData.jf += match.player2Score;
+        newData.jc += match.player1Score;
+        newData.dif += match.player2Score - match.player1Score;
+      }
+
+      if (
+        match.player1Name !== playerData.name &&
+        match.player2Name !== playerData.name
+      ) {
+        newData;
+      }
+    });
+
+    const winningPoints: number = newData.pg * 3;
+    const losingPoints: number = newData.pp * 1;
+    const rankingPoints: number = winningPoints + losingPoints;
+
+    return { newData, matchScore: updatedMatches, rankingPoints };
+  });
+
+  const sortedPlayersByRanking = playersDataToShow.sort(
+    (a, b) => b.rankingPoints - a.rankingPoints
+  );
+
+  console.log(sortedPlayersByRanking);
 
   return (
     <table className="w-full md:w-8/12 xl:w-5/12">
@@ -52,7 +118,11 @@ export default function Table({ playersData }: { playersData: PlayerProps[] }) {
           <th>Dif</th>
         </tr>
       </thead>
-      <tbody>{playersDataToShow}</tbody>
+      <tbody>
+        {sortedPlayersByRanking.map(({ newData }, index) => (
+          <Player key={index} playerData={newData} ranking={index + 1} />
+        ))}
+      </tbody>
     </table>
   );
 }
