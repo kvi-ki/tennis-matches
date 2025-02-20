29d74b636dc3476c8011e71026627e64635fdde0
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-02-20 15:40:19 +0100

    feat: use reduce method instead of forEach

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -23,73 +23,74 @@ export default function Table({ playersData }: { playersData: PlayerProps[] }) {
       (match) => match.player1Score > 0 || match.player2Score > 0
     );
 
-    let newData = {
-      ...playerData,
-      pj: playerData.pj || 0,
-      pg: playerData.pg || 0,
-      pp: playerData.pp || 0,
-      jf: playerData.jf || 0,
-      jc: playerData.jc || 0,
-      dif: playerData.dif || 0
-    };
+    const newData = updatedMatches.reduce(
+      (data: PlayerProps, match: MatchScoreProps) => {
+        const winnerPlayer1 =
+          match.theWinner === playerData.name &&
+          match.player1Name === playerData.name;
+        const winnerPlayer2 =
+          match.theWinner === playerData.name &&
+          match.player2Name === playerData.name;
+        const notWinnerPlayer1 =
+          match.theWinner !== playerData.name &&
+          match.player1Name === playerData.name;
+        const notWinnerPlayer2 =
+          match.theWinner !== playerData.name &&
+          match.player2Name === playerData.name;
 
-    updatedMatches.forEach((match: MatchScoreProps) => {
-      const winnerPlayer1 =
-        match.theWinner === playerData.name &&
-        match.player1Name === playerData.name;
-      const winnerPlayer2 =
-        match.theWinner === playerData.name &&
-        match.player2Name === playerData.name;
-      const notWinnerPlayer1 =
-        match.theWinner !== playerData.name &&
-        match.player1Name === playerData.name;
-      const notWinnerPlayer2 =
-        match.theWinner !== playerData.name &&
-        match.player2Name === playerData.name;
+        if (winnerPlayer1) {
+          return {
+            ...data,
+            pj: (data.pj += 1),
+            pg: (data.pg += 1),
+            pp: data.pp,
+            jf: (data.jf += match.player1Score),
+            jc: (data.jc += match.player2Score),
+            dif: (data.dif += match.player1Score - match.player2Score)
+          };
+        }
 
-      if (winnerPlayer1) {
-        newData.pj += 1;
-        newData.pg += 1;
-        newData.pp;
-        newData.jf += match.player1Score;
-        newData.jc += match.player2Score;
-        newData.dif += match.player1Score - match.player2Score;
-      }
+        if (winnerPlayer2) {
+          return {
+            ...data,
+            pj: (data.pj += 1),
+            pg: (data.pg += 1),
+            pp: data.pp,
+            jf: (data.jf += match.player2Score),
+            jc: (data.jc += match.player1Score),
+            dif: (data.dif += match.player2Score - match.player1Score)
+          };
+        }
 
-      if (winnerPlayer2) {
-        newData.pj += 1;
-        newData.pg += 1;
-        newData.pp;
-        newData.jf += match.player2Score;
-        newData.jc += match.player1Score;
-        newData.dif += match.player2Score - match.player1Score;
-      }
+        if (notWinnerPlayer1) {
+          return {
+            ...data,
+            pj: (data.pj += 1),
+            pg: data.pg,
+            pp: (data.pp += 1),
+            jf: (data.jf += match.player1Score),
+            jc: (data.jc += match.player2Score),
+            dif: (data.dif += match.player1Score - match.player2Score)
+          };
+        }
 
-      if (notWinnerPlayer1) {
-        newData.pj += 1;
-        newData.pg;
-        newData.pp += 1;
-        newData.jf += match.player1Score;
-        newData.jc += match.player2Score;
-        newData.dif += match.player1Score - match.player2Score;
-      }
+        if (notWinnerPlayer2) {
+          return {
+            ...data,
+            pj: (data.pj += 1),
+            pg: data.pg,
+            pp: (data.pp += 1),
+            jf: (data.jf += match.player2Score),
+            jc: (data.jc += match.player1Score),
+            dif: (data.dif += match.player2Score - match.player1Score)
+          };
+        }
 
-      if (notWinnerPlayer2) {
-        newData.pj += 1;
-        newData.pg;
-        newData.pp += 1;
-        newData.jf += match.player2Score;
-        newData.jc += match.player1Score;
-        newData.dif += match.player2Score - match.player1Score;
-      }
+        return data;
+      },
 
-      if (
-        match.player1Name !== playerData.name &&
-        match.player2Name !== playerData.name
-      ) {
-        newData;
-      }
-    });
+      { ...playerData, pj: 0, pg: 0, pp: 0, jf: 0, jc: 0, dif: 0 }
+    );
 
     const winningPoints: number = newData.pg * 3;
     const losingPoints: number = newData.pp * 1;
