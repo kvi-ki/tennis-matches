7a87d1bec692cd1b1dcafbc6afa76f5abc49935f
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-04-28 13:12:05 +0200

    feat: use MonthlyBoard component in menu of the App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,23 @@
 import data from '../../public/leagueData.json';
 import React from 'react';
 import DivisionCards from '@/components/divisionCards/DivisionCards';
+import MonthlyBoard from '@/components/monthlyBoard.tsx/MonthlyBoard';
 
 function App() {
   return (
     <main>
-      <DivisionCards leagueData={data} />
+      <menu className="m-4 flex justify-center">
+        <ul className="flex gap-4">
+          {data.map((data) => {
+            return (
+              <li>
+                <MonthlyBoard monthData={data} />
+              </li>
+            );
+          })}
+        </ul>
+      </menu>
+      <DivisionCards leagueData={data[2].monthData} />
     </main>
   );
 }
