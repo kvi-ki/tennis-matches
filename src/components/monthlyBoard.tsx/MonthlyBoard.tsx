8d090162ac757e30efcd68039371e9f582b12610
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-05 14:48:23 +0200

    style: update the button style if the button is selected

--- src/components/monthlyBoard.tsx/MonthlyBoard.tsx
+++ src/components/monthlyBoard.tsx/MonthlyBoard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { LeagueDataProps } from '../leagueData/LeagueData';
 
 export type MonthlyDataProps = {
@@ -7,19 +8,21 @@ export type MonthlyDataProps = {
 
 export default function MonthlyBoard({
   monthData,
+  selectedMonth,
   onClick
 }: {
   monthData: MonthlyDataProps;
+  selectedMonth: string;
   onClick: (event: any) => void;
 }) {
+  const monthIsSelected = selectedMonth === monthData.month;
+
   return (
-    <div className="mt-4">
-      <button
-        onClick={onClick}
-        className="w-20 border border-navy text-navy text-textMd text-center text-semibold hover:text-green cursor-pointer md:w-28"
-      >
-        {monthData.month}
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      className={`${monthIsSelected ? 'bg-green text-white hover:bg-navy' : 'bg-white text-green hover:bg-navy hover:text-white'} mt-4 w-20 border border-navy text-textMd text-center text-semibold  cursor-pointer md:w-28`}
+    >
+      {monthData.month}
+    </button>
   );
 }
