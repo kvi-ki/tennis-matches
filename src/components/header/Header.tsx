b0bb1ed38f6615c33b894cb644741650a21885d3
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-06 15:00:00 +0200

    style: update color and header size

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header({ divisionNumber }: { divisionNumber: string }) {
   return (
-    <h1 className="text-header text-navy text-center m-4 xl:text-headerLg">
+    <h1 className="text-header text-navy text-center m-4">
       División {divisionNumber}
     </h1>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,12 +20,12 @@ export default {
       navy: '#0461a6',
       blue: '#0089c1',
       gray: "#7D8080",
-      darkGray: '#3f4756',
+      darkGray: '#787777',
+      lightGray: '#faf9f8',
       white: '#FFFFFF'
     },
     fontSize: {
       header: '2rem',
-      headerLg: '2.5rem',
       header2: '1.3rem',
       header2Lg: '2rem',
       textSm: '0.6rem',
