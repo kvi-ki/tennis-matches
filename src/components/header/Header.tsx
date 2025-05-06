f1081567e536bcdceb1daddf55fee5262f3f0a83
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-06 12:53:41 +0200

    style: update margin for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
-export default function Header({divisionNumber} : {divisionNumber: string} ) {
+export default function Header({ divisionNumber }: { divisionNumber: string }) {
   return (
-    <h1 className="text-header text-navy text-center mb-4 lg:text-headerLg">
+    <h1 className="text-header text-navy text-center m-4 xl:text-headerLg">
       División {divisionNumber}
     </h1>
   );
