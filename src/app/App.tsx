1b1d7f7ef5268e7e0714b1bbd0352cca10110c4c
Victoria Kovaleva <victoriak.dev@gmail.com>
2025-05-08 11:52:12 +0200

    style: minor change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -49,7 +49,7 @@ function App() {
 
   return (
     <main className="flex flex-col items-center">
-      <menu className="w-full m-4 bg-white fixed top-0">
+      <menu className="w-full m-4 bg-white fixed top-0 z-99">
         <ul className="flex justify-center gap-4">
           {formattedData.map((data) => {
             return (
